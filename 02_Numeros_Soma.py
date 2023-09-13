6341c4914e1084cd01b4e416908316fed5fbff72
gustavomplima <ogustavolima@gmail.com>
2023-09-12 23:51:09 -0300

    Exercício 02: Informe dois números e mostre a soma

---
+++ 02_Numeros_Soma.py
@@ -0,0 +1,11 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = input("Digite um número: ")
+numero2 = input("Digite mais um número: ")
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+soma = numero1 + numero2
+
+print("A Soma dos números digitados é: " + str(soma))
